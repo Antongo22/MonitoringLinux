8ab4fe567d518971f258756ed141ce664d8dbb4f
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-06-07 01:06:56 +0300

    сука, я ебал линукс

--- MonitoringLinux/MonitoringLinux.cpp
+++ MonitoringLinux/MonitoringLinux.cpp
@@ -1,20 +1,138 @@
-﻿// MonitoringLinux.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <comdef.h>
+#include <Wbemidl.h>
 
-#include <iostream>
+#pragma comment(lib, "wbemuuid.lib")
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    HRESULT hres;
+
+    // Инициализация COM
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize COM library." << std::endl;
+        return 1;
+    }
+
+    // Инициализация безопасности
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,
+        NULL,
+        NULL,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL,
+        EOAC_NONE,
+        NULL
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize security." << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+
+    // Получение указателя на интерфейс сервиса WMI
+    IWbemLocator* pLoc = NULL;
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator,
+        (LPVOID*)&pLoc
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to create IWbemLocator object." << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+
+    // Подключение к сервису WMI
+    IWbemServices* pSvc = NULL;
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        NULL,
+        NULL,
+        0,
+        NULL,
+        0,
+        0,
+        &pSvc
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to connect to WMI service." << std::endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    // Установка контекста безопасности для вызова методов
+    hres = CoSetProxyBlanket(
+        pSvc,
+        RPC_C_AUTHN_WINNT,
+        RPC_C_AUTHZ_NONE,
+        NULL,
+        RPC_C_AUTHN_LEVEL_CALL,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL,
+        EOAC_NONE
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to set proxy blanket." << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    // Получение нагрузки на процессор
+    while (true) {
+        IEnumWbemClassObject* pEnumerator = NULL;
+        hres = pSvc->ExecQuery(
+            bstr_t("WQL"),
+            bstr_t("SELECT LoadPercentage FROM Win32_Processor"),
+            WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+            NULL,
+            &pEnumerator
+        );
+        if (FAILED(hres)) {
+            std::cerr << "Failed to execute WMI query." << std::endl;
+            pSvc->Release();
+            pLoc->Release();
+            CoUninitialize();
+            return 1;
+        }
+
+        IWbemClassObject* pclsObj = NULL;
+        ULONG uReturn = 0;
+
+        while (pEnumerator) {
+            HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+            if (0 == uReturn)
+                break;
+
+            VARIANT vtProp;
+            hr = pclsObj->Get(L"LoadPercentage", 0, &vtProp, 0, 0);
+            if (SUCCEEDED(hr) && vtProp.vt == VT_UINT) {
+                std::cout << "Total CPU Load: " << vtProp.uintVal << "%" << std::endl;
+            }
+            else {
+                std::cerr << "Failed to get CPU load value." << std::endl;
+            }
+
+            VariantClear(&vtProp);
+            pclsObj->Release();
+        }
+
+        pEnumerator->Release();
+
+        Sleep(1000); // Ожидание в миллисекундах между обновлениями информации о загрузке
+    }
+
+    // Освобождение ресурсов
+    pSvc->Release();
+    pLoc->Release();
+    CoUninitialize();
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
