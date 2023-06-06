76570013b9b6e453bc4dda14c4f50ba0390d6a6f
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-06-07 01:07:15 +0300

    x2

--- MonitoringLinux/MonitoringLinux.cpp
+++ MonitoringLinux/MonitoringLinux.cpp
@@ -1,138 +1,42 @@
 ﻿#include <iostream>
-#include <comdef.h>
-#include <Wbemidl.h>
+#include <fstream>
+#include <string>
+#include <unistd.h>
 
-#pragma comment(lib, "wbemuuid.lib")
+using namespace std;
+
+float getCpuLoad() {
+    ifstream statFile("/proc/stat");
+    string line;
+    getline(statFile, line);
+    statFile.close();
+
+    size_t pos = line.find("cpu");
+    if (pos == string::npos)
+        return -1;
+
+    line = line.substr(pos + 3);  // Skip "cpu" prefix
+
+    int user, nice, system, idle;
+    sscanf(line.c_str(), "%d %d %d %d", &user, &nice, &system, &idle);
+
+    float total = user + nice + system + idle;
+    float cpuLoad = (total - idle) / total * 100.0;
+
+    return cpuLoad;
+}
 
 int main() {
-    HRESULT hres;
-
-    // Инициализация COM
-    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-    if (FAILED(hres)) {
-        std::cerr << "Failed to initialize COM library." << std::endl;
-        return 1;
-    }
-
-    // Инициализация безопасности
-    hres = CoInitializeSecurity(
-        NULL,
-        -1,
-        NULL,
-        NULL,
-        RPC_C_AUTHN_LEVEL_DEFAULT,
-        RPC_C_IMP_LEVEL_IMPERSONATE,
-        NULL,
-        EOAC_NONE,
-        NULL
-    );
-    if (FAILED(hres)) {
-        std::cerr << "Failed to initialize security." << std::endl;
-        CoUninitialize();
-        return 1;
-    }
-
-    // Получение указателя на интерфейс сервиса WMI
-    IWbemLocator* pLoc = NULL;
-    hres = CoCreateInstance(
-        CLSID_WbemLocator,
-        0,
-        CLSCTX_INPROC_SERVER,
-        IID_IWbemLocator,
-        (LPVOID*)&pLoc
-    );
-    if (FAILED(hres)) {
-        std::cerr << "Failed to create IWbemLocator object." << std::endl;
-        CoUninitialize();
-        return 1;
-    }
-
-    // Подключение к сервису WMI
-    IWbemServices* pSvc = NULL;
-    hres = pLoc->ConnectServer(
-        _bstr_t(L"ROOT\\CIMV2"),
-        NULL,
-        NULL,
-        0,
-        NULL,
-        0,
-        0,
-        &pSvc
-    );
-    if (FAILED(hres)) {
-        std::cerr << "Failed to connect to WMI service." << std::endl;
-        pLoc->Release();
-        CoUninitialize();
-        return 1;
-    }
-
-    // Установка контекста безопасности для вызова методов
-    hres = CoSetProxyBlanket(
-        pSvc,
-        RPC_C_AUTHN_WINNT,
-        RPC_C_AUTHZ_NONE,
-        NULL,
-        RPC_C_AUTHN_LEVEL_CALL,
-        RPC_C_IMP_LEVEL_IMPERSONATE,
-        NULL,
-        EOAC_NONE
-    );
-    if (FAILED(hres)) {
-        std::cerr << "Failed to set proxy blanket." << std::endl;
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return 1;
-    }
-
-    // Получение нагрузки на процессор
     while (true) {
-        IEnumWbemClassObject* pEnumerator = NULL;
-        hres = pSvc->ExecQuery(
-            bstr_t("WQL"),
-            bstr_t("SELECT LoadPercentage FROM Win32_Processor"),
-            WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-            NULL,
-            &pEnumerator
-        );
-        if (FAILED(hres)) {
-            std::cerr << "Failed to execute WMI query." << std::endl;
-            pSvc->Release();
-            pLoc->Release();
-            CoUninitialize();
-            return 1;
+        float cpuLoad = getCpuLoad();
+        if (cpuLoad == -1) {
+            cout << "Failed to read CPU load." << endl;
         }
-
-        IWbemClassObject* pclsObj = NULL;
-        ULONG uReturn = 0;
-
-        while (pEnumerator) {
-            HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
-            if (0 == uReturn)
-                break;
-
-            VARIANT vtProp;
-            hr = pclsObj->Get(L"LoadPercentage", 0, &vtProp, 0, 0);
-            if (SUCCEEDED(hr) && vtProp.vt == VT_UINT) {
-                std::cout << "Total CPU Load: " << vtProp.uintVal << "%" << std::endl;
-            }
-            else {
-                std::cerr << "Failed to get CPU load value." << std::endl;
-            }
-
-            VariantClear(&vtProp);
-            pclsObj->Release();
+        else {
+            cout << "CPU Load: " << cpuLoad << "%" << endl;
         }
-
-        pEnumerator->Release();
-
-        Sleep(1000); // Ожидание в миллисекундах между обновлениями информации о загрузке
+        usleep(1000000);  // Pause for 1 second
     }
-
-    // Освобождение ресурсов
-    pSvc->Release();
-    pLoc->Release();
-    CoUninitialize();
 
     return 0;
 }
