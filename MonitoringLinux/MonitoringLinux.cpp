3412224bbf4dc12eda5aff8f9a90f72a57643df3
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-06-10 19:48:38 +0300

    для проца есть

--- MonitoringLinux/MonitoringLinux.cpp
+++ MonitoringLinux/MonitoringLinux.cpp
@@ -5,42 +5,53 @@
 
 using namespace std;
 
-float getCpuLoad() {
-    ifstream statFile("/proc/stat");
-    string line;
-    getline(statFile, line);
-    statFile.close();
+class Computer {
+private:
+    ifstream statFile;
 
-    size_t pos = line.find("cpu");
-    if (pos == string::npos)
-        return -1;
+public:
+    float getCpuLoad() {
+        statFile.open("/proc/stat");
+        string line;
+        getline(statFile, line);
+        statFile.close();
 
-    line = line.substr(pos + 4);  // Skip "cpu " prefix
+        size_t pos = line.find("cpu");
+        if (pos == string::npos)
+            return -1;
 
-    float user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
-    sscanf(line.c_str(), "%f %f %f %f %f %f %f %f %f %f",
-        &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
+        line = line.substr(pos + 4);  // Удаляем "cpu " префикс
 
-    float idleTicks = idle + iowait;
-    float systemTicks = system + irq + softirq;
-    float totalTicks = idleTicks + systemTicks + user + nice + steal;
+        float user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+        sscanf(line.c_str(), "%f %f %f %f %f %f %f %f %f %f",
+            &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
 
-    float cpuLoad = (totalTicks - idleTicks) / totalTicks * 100.0;
+        float idleTicks = idle + iowait;
+        float systemTicks = system + irq + softirq;
+        float totalTicks = idleTicks + systemTicks + user + nice + steal;
 
-    return cpuLoad;
-}
+        float cpuLoad = (totalTicks - idleTicks) / totalTicks * 100.0;
+
+        return cpuLoad;
+    }
+
+    void monitorCpuLoad() {
+        while (true) {
+            float cpuLoad = getCpuLoad();
+            if (cpuLoad == -1) {
+                cout << "Failed to read CPU load." << endl;
+            }
+            else {
+                cout << "CPU Load: " << cpuLoad << "%" << endl;
+            }
+            usleep(1000000);  // Пауза на 1 секунду 
+        }
+    }
+};
 
 int main() {
-    while (true) {
-        float cpuLoad = getCpuLoad();
-        if (cpuLoad == -1) {
-            cout << "Failed to read CPU load." << endl;
-        }
-        else {
-            cout << "CPU Load: " << cpuLoad << "%" << endl;
-        }
-        usleep(1000000);  // Pause for 1 second
-    }
+    Computer computer;
+    computer.monitorCpuLoad();
 
     return 0;
 }
